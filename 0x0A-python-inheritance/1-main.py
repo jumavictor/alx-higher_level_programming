fa6efbb92cf0eca4d46177f08dd87eb4414cab05
Jumavictor <victorjuma48@gmail.com>
2023-02-06 01:43:18 -0800

    Task 2. a function that returns True if the object is exactly an instance of the specified class ; otherwise False.

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -2,7 +2,11 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(7)
+my_list.append(1)
+my_list.append(2)
+my_list.append(4)
+my_list.append(3)
+my_list.append(5)
 # my_list.append('-c')
 # my_list.append('-z')
 # my_list.append('-g')
